e1d184da5da9b91dcba8e73bfa094621806145bf
sano11o1 <mashio.tech@gmail.com>
2022-09-06 09:58:17 +0900

    sqs!!

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/sano11o1/lambda-sqs-sample
 
 go 1.18
+
+require github.com/aws/aws-sdk-go v1.44.91
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sqs"
+)
 
 func main() {
-	fmt.Println("SQS SQS SQS!")
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+	svc := sqs.New(sess)
+
+	queueUrl := "https://sqs.ap-northeast-1.amazonaws.com/087391343737/MyQueue"
+
+	_, err := svc.SendMessage(&sqs.SendMessageInput{
+		DelaySeconds: aws.Int64(10),
+		MessageAttributes: map[string]*sqs.MessageAttributeValue{
+			"Title": &sqs.MessageAttributeValue{
+				DataType:    aws.String("String"),
+				StringValue: aws.String("The Whistler"),
+			},
+			"Author": &sqs.MessageAttributeValue{
+				DataType:    aws.String("String"),
+				StringValue: aws.String("John Grisham"),
+			},
+			"WeeksOn": &sqs.MessageAttributeValue{
+				DataType:    aws.String("Number"),
+				StringValue: aws.String("6"),
+			},
+		},
+		MessageBody: aws.String("Information about current NY Times fiction bestseller for week of 12/11/2016."),
+		QueueUrl:    &queueUrl,
+	})
+
+	if err != nil {
+		fmt.Println("エラー", err)
+	}
 }
